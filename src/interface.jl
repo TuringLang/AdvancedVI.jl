37189780b35261614c0b2ef5344523005aaab7a1
Theo Galy-Fajou <theo.galyfajou@gmail.com>
2021-08-24 15:13:45 +0200

    Remade the interface to take function plus arguments

--- src/interface.jl
+++ src/interface.jl
@@ -1,6 +1,5 @@
 """
-    vi(vo::VariationalObjective, model, alg::VariationalInference, q::VariationalPosterior; opt, hyperparams, opt_hyperparams)
-    vi(model, alg::VariationalInference, q::VariationalPosterior; opt, hyperparams, opt_hyperparams)
+    vi([rng::AbstractRNG, [vo::VariationalObjective]], model, alg::VariationalInference, q::VariationalPosterior; opt, hyperparams, opt_hyperparams)::VariationalPosterior
 
 Constructs the variational posterior from the `model` and performs the optimization
 following the configuration of the given `VariationalInference` instance.
@@ -13,7 +12,48 @@ following the configuration of the given `VariationalInference` instance.
 - `opt` : Optimiser (from `Flux.Optimise`) used to update the variational parameters
 - `hyperparams` : Hyperparameters, if different than nothing, `model(hyperparams)` will be called to obatin the logjoint
 - `opt_hyperparams` : Optimiser for the Hyperparameters
+
+    vi([rng::AbstractRNG, [vo::VariationalObjective]], model, alg::VariationalInference, q::Function, θ::AbstractVector; opt, hyperparams, opt_hyperparams)::AbstractVector
+
+Constructs the variational posterior from the `model` and performs the optimization
+following the configuration of the given `VariationalInference` instance.
+## Arguments
+- `vo` : `VariationalObjective`, `ELBO()` by default
+- `model`: `Turing.Model` or `Function` z ↦ log p(x, z) where `x` denotes the observations
+- `alg`: the VI algorithm used
+- `q`: a function creating a distribution from the parameters `θ`
+- `θ`: the variational parameters
+## Keyword Arguments
+- `opt` : Optimiser (from `Flux.Optimise`) used to update the variational parameters
+- `hyperparams` : Hyperparameters, if different than nothing, `model(hyperparams)` will be called to obatin the logjoint
+- `opt_hyperparams` : Optimiser for the Hyperparameters
+
 """
+function vi(
+    rng::AbstractRNG,
+    vo::VariationalObjective,
+    model,
+    alg::VariationalInference,
+    q;
+    opt=TruncatedADAGrad(),
+    hyperparams=nothing,
+    opt_hyperparams=nothing,
+)
+    θ, to_dist = flatten(q)
+    θ = vi(
+        rng,
+        vo,
+        alg,
+        to_dist,
+        θ,
+        model;
+        opt=opt,
+        hyperparams=hyperparams,
+        opt_hyperparams=opt_hyperparams,
+    )
+    return to_dist(θ)
+end
+
 function vi(
     vo::VariationalObjective,
     model,
@@ -59,23 +99,70 @@ function vi(
     vo::VariationalObjective,
     model,
     alg::VariationalInference,
-    q;
+    q,
+    θ::AbstractVector;
     opt=TruncatedADAGrad(),
     hyperparams=nothing,
     opt_hyperparams=nothing,
 )
-    check_compatibility(alg, q)
     return optimize!(
         rng,
         vo,
         alg,
         q,
+        θ,
         model;
         opt=opt,
         hyperparams=hyperparams,
         opt_hyperparams=opt_hyperparams,
     )
 end
+
+function vi(
+    vo::VariationalObjective,
+    model,
+    alg::VariationalInference,
+    q,
+    θ::AbstractVector;
+    opt=TruncatedADAGrad(),
+    hyperparams=nothing,
+    opt_hyperparams=nothing,
+)
+    return vi(
+        GLOBAL_RNG,
+        vo,
+        model,
+        alg,
+        q,
+        θ;
+        opt=opt,
+        hyperparams=hyperparams,
+        opt_hyperparams=opt_hyperparams,
+    )
+end
+
+function vi(
+    model,
+    alg::VariationalInference,
+    q,
+    θ;
+    opt=TruncatedADAGrad(),
+    hyperparams=nothing,
+    opt_hyperparams=nothing,
+)
+    return vi(
+        ELBO(),
+        model,
+        alg,
+        q,
+        θ;
+        opt=opt,
+        hyperparams=hyperparams,
+        opt_hyperparams=opt_hyperparams,
+    )
+end
+
+
 
 """
     optimize!([vo::VariationalObjective, [alg::VariationalInference{AD}, q::VariationalPosterior, model::Model], θ], ]; optimizer = TruncatedADAGrad())
@@ -87,7 +174,8 @@ function optimize!(
     rng::AbstractRNG,
     vo::VariationalObjective,
     alg::VariationalInference,
-    q,
+    to_dist,
+    θ,
     model;
     opt=TruncatedADAGrad(),
     hyperparams=nothing,
@@ -95,7 +183,7 @@ function optimize!(
 )
     max_iters = maxiters(alg)
 
-    state = init(rng, alg, q, opt) # opt is there to be used in the future
+    state = init(rng, alg, to_dist, θ, opt) # opt is there to be used in the future
 
     i = 0
     prog = if PROGRESS[]
@@ -107,7 +195,7 @@ function optimize!(
     # add criterion? A running mean maybe?
     time_elapsed = @elapsed while (i < max_iters) # & converged
         logπ = makelogπ(model, hyperparams)
-        step!(rng, vo, alg, q, logπ, state, opt)
+        step!(rng, vo, alg, to_dist, θ, logπ, state, opt)
 
         # For debugging this would need to be updated somehow
         # AdvancedVI.DEBUG && @debug "Step $i" Δ DiffResults.value(diff_result)
@@ -116,29 +204,5 @@ function optimize!(
         i += 1
     end
 
-    return final_dist(alg, q, state)
-end
-
-## Return the final distribution after training
-final_dist(alg, q, state) = q
-
-## Verify that the algorithm can work with the corresponding variational distribution
-function check_compatibility(alg, q)
-    if !compat(alg, q)
-        throw(
-            ArgumentError(
-                "Algorithm $(alg) cannot work with distributions of type $(typeof(q)), compatible distributions are: $(compats(alg))",
-            ),
-        )
-    end
-end
-
-function compat(alg::VariationalInference, q)
-    return q isa compats(alg)
-end
-
-## Return the compatible distribution types for a given algorithm
-## By default all pass
-function compats(::VariationalInference)
-    return Any
+    return θ
 end
