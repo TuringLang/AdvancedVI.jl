88e0b79758c2f207b9d3c7120b469af837049fec
Ray Kim <msca8h@naver.com>
2023-03-14 19:56:47 +0000

    remove unused file, remove unused dependency

--- src/objectives.jl
+++
@@ -1,7 +0,0 @@
-struct ELBO <: VariationalObjective end
-
-function (elbo::ELBO)(alg, q, logπ, num_samples; kwargs...)
-    return elbo(Random.default_rng(), alg, q, logπ, num_samples; kwargs...)
-end
-
-const elbo = ELBO()
