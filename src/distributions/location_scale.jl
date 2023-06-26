e6a082aadbd3fa92e60fedf5373f2efbb1875ecc
Ray Kim <msca8h@naver.com>
2023-06-26 23:47:04 +0100

    fix Location Scale to not depend on Bijectors

--- src/distributions/location_scale.jl
+++ src/distributions/location_scale.jl
@@ -1,58 +1,79 @@
 
-import Base: rand, _rand!
+struct VILocationScale{L, S, D, R} <: ContinuousMultivariateDistribution
+    location::L
+    scale   ::S
+    dist    ::D
+    epsilon ::R
 
-struct LocationScale{ReparamMvDist <: Bijectors.TransformedDistribution} <: ContinuousMultivariateDistribution
-    q_trans::ReparamMvDist
-
-    function LocationScale(μ::AbstractVector,
-                           L::Union{<: AbstractTriangular,
-                                    <: Diagonal},
-                           q₀::ContinuousMultivariateDistribution)
+    function VILocationScale(μ::AbstractVector{<:Real},
+                             L::Union{<:AbstractTriangular{<:Real},
+                                      <:Diagonal{<:Real}},
+                             q_base::ContinuousUnivariateDistribution,
+                             epsilon::Real)
+        # Restricting all the arguments to have the same types creates problems 
+        # with dual-variable-based AD frameworks.
         @assert (length(μ) == size(L,1)) && (length(μ) == size(L,2))
-        q_trans = transformed(q₀, Bijectors.Shift(μ) ∘ Bijectors.Scale(L))       
-        new{typeof(q_trans)}(q_trans)
-    end
-
-    function LocationScale(q_trans::Bijectors.TransformedDistribution)
-        new{typeof(q_trans)}(q_trans)
+        new{typeof(μ), typeof(L), typeof(q_base), typeof(epsilon)}(μ, L, q_base, epsilon)
     end
 end
 
-Functors.@functor LocationScale
+Functors.@functor VILocationScale (location, scale)
 
-Base.length(q::LocationScale) = length(q.q_trans)
-Base.size(q::LocationScale) = size(q.q_trans)
+Base.length(q::VILocationScale) = length(q.location)
+Base.size(q::VILocationScale) = size(q.location)
 
-function StatsBase.entropy(q::LocationScale)
-    q_base = q.q_trans.dist
-    scale  = q.q_trans.transform.inner.a
-    entropy(q_base) + first(logabsdet(scale))
+function StatsBase.entropy(q::VILocationScale)
+    @unpack location, scale, dist = q
+    n_dims = length(location)
+    n_dims*entropy(dist) + first(logabsdet(scale))
 end
 
-
-Distributions.logpdf(q::LocationScale, z::AbstractVector) = logpdf(q.q_trans, z)
-
-_logpdf(q::LocationScale, y::AbstractVector) = _logpdf(q.q_trans, y)
-
-rand(q::LocationScale) = rand(q.q_trans)
-
-rand(rng::Random.AbstractRNG, q::LocationScale, num_samples::Int) = rand(rng, q.q_trans, num_samples)
-
-_rand!(rng::Random.AbstractRNG, q::LocationScale, x::AbstractVector{<:Real}) = _rand!(rng, q.q_trans, x)
-
-
-function FullRankGaussian(μ::AbstractVector{T},
-                          L::AbstractTriangular{T,S}) where {T <: Real, S}
-    @assert (length(μ) == size(L,1)) && (length(μ) == size(L,2))
-    n_dims = length(μ)
-    q_base = MvNormal(FillArrays.Zeros{T}(n_dims), PDMats.ScalMat{T}(n_dims, one(T)))
-    LocationScale(μ, L, q_base)
+function logpdf(q::VILocationScale, z::AbstractVector{<:Real})
+    @unpack location, scale, dist = q
+    mapreduce(zᵢ -> logpdf(dist, zᵢ), +, scale \ (z - location)) + first(logabsdet(scale))
 end
 
-function MeanFieldGaussian(μ::AbstractVector{T},
-                           L::Diagonal{T,V}) where {T <: Real, V}
-    @assert (length(μ) == size(L,1))
-    n_dims = length(μ)
-    q_base = MvNormal(FillArrays.Zeros{T}(n_dims), PDMats.ScalMat{T}(n_dims, one(T)))
-    LocationScale(μ, L, q_base)
+function _logpdf(q::VILocationScale, z::AbstractVector{<:Real})
+    @unpack location, scale, dist = q
+    mapreduce(zᵢ -> logpdf(dist, zᵢ), +, scale \ (z - location)) + first(logabsdet(scale))
+end
+
+function rand(q::VILocationScale)
+    @unpack location, scale, dist = q
+    n_dims = length(location)
+    scale*rand(dist, n_dims) + location
+end
+
+function rand(rng::AbstractRNG, q::VILocationScale, num_samples::Int) 
+    @unpack location, scale, dist = q
+    n_dims = length(location)
+    scale*rand(dist, n_dims, num_samples) .+ location
+end
+
+function _rand!(rng::AbstractRNG, q::VILocationScale, x::AbstractVector{<:Real})
+    @unpack location, scale, dist = q
+    rand!(rng, dist, x)
+    x .= scale*x
+    return x += location
+end
+
+function _rand!(rng::AbstractRNG, q::VILocationScale, x::AbstractMatrix{<:Real})
+    @unpack location, scale, dist = q
+    rand!(rng, dist, x)
+    x *= scale
+    return x += location
+end
+
+function VIFullRankGaussian(μ::AbstractVector{T},
+                            L::AbstractTriangular{T},
+                            epsilon::Real = eps(T)) where {T <: Real}
+    q_base = Normal{T}(zero(T), one(T))
+    VILocationScale(μ, L, q_base, epsilon)
+end
+
+function VIMeanFieldGaussian(μ::AbstractVector{T},
+                             L::Diagonal{T},
+                             epsilon::Real = eps(T)) where {T <: Real}
+    q_base = Normal{T}(zero(T), one(T))
+    VILocationScale(μ, L, q_base, epsilon)
 end
