0f6f6a429ba74e491943ad96fa52ff9f897cc862
Ray Kim <msca8h@naver.com>
2023-07-14 03:04:35 +0100

    add more tests, fix seed for tests

--- test/distributions.jl
+++ test/distributions.jl
@@ -8,17 +8,19 @@ using Distributions: _logpdf
         covtype  = [:meanfield, :fullrank],
         realtype = [Float32,     Float64]
 
+        seed         = (0x38bef07cf9cc549d, 0x49e2430080b3f797)
+        rng          = Philox4x(UInt64, seed, 8)
         realtype     = Float64
         ϵ            = 1e-2
         n_dims       = 10
         n_montecarlo = 1000_000
 
-        μ  = randn(realtype, n_dims)
-        L₀ = randn(realtype, n_dims, n_dims) |> LowerTriangular
+        μ  = randn(rng, realtype, n_dims)
+        L₀ = randn(rng, realtype, n_dims, n_dims) |> LowerTriangular
         Σ  = if covtype == :fullrank
             Σ = (L₀*L₀' + ϵ*I) |> Hermitian
         else
-            Diagonal(exp.(randn(realtype, n_dims)))
+            Diagonal(log.(exp.(randn(rng, realtype, n_dims)) .+ 1))
         end
 
         L = cholesky(Σ).L
@@ -31,15 +33,26 @@ using Distributions: _logpdf
             MvNormal(μ, Σ)
         end
 
-        z = randn(n_dims)
-        @test logpdf(q, z)  ≈ logpdf(q_true, z)
-        @test _logpdf(q, z) ≈ _logpdf(q_true, z)
-        @test entropy(q)    ≈ entropy(q_true)
+        @testset "logpdf" begin
+            z = randn(rng, realtype, n_dims)
+            @test logpdf(q, z)  ≈ logpdf(q_true, z)
+            @test _logpdf(q, z) ≈ _logpdf(q_true, z)
+            @test eltype(logpdf(q, z))  == realtype
+            @test eltype(_logpdf(q, z)) == realtype
+        end
 
-        z_samples  = rand(q, n_montecarlo)
-        threesigma = L
-        @test dropdims(mean(z_samples, dims=2), dims=2) ≈ μ       rtol=realtype(1e-2)
-        @test dropdims(var(z_samples, dims=2),  dims=2) ≈ diag(Σ) rtol=realtype(1e-2)
-        @test cov(z_samples, dims=2)                    ≈ Σ       rtol=realtype(1e-2)
+        @testset "entropy" begin
+            @test eltype(entropy(q)) == realtype
+            @test entropy(q)         ≈ entropy(q_true)
+        end
+
+        @testset "sampling" begin
+            z_samples  = rand(rng, q, n_montecarlo)
+            threesigma = L
+            @test eltype(z_samples) == realtype
+            @test dropdims(mean(z_samples, dims=2), dims=2) ≈ μ       rtol=realtype(1e-2)
+            @test dropdims(var(z_samples, dims=2),  dims=2) ≈ diag(Σ) rtol=realtype(1e-2)
+            @test cov(z_samples, dims=2)                    ≈ Σ       rtol=realtype(1e-2)
+        end
     end
 end
