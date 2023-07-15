619b1c05eaf669491f82406becb9a31dba1871cc
Ray Kim <msca8h@naver.com>
2023-07-15 22:34:32 +0100

    remove unused epsilon argument in location scale

--- src/distributions/location_scale.jl
+++ src/distributions/location_scale.jl
@@ -1,31 +1,31 @@
 
 """
+    VILocationScale{L,R,D}(location::L, scale::S, dist::D) <: ContinuousMultivariateDistribution
 
 The [location scale] variational family broadly represents various variational
 families using `location` and `scale` variational parameters.
 
-Multivariate Student-t variational family with ``\\nu``-degrees of freedom can
-be constructed as:
+It generally represents any distribution for which the sampling path can be
+represented as the following:
 ```julia
-q₀ = VILocationScale(μ, L, StudentT(ν), eps(Float32))
+  d = length(location)
+  u = rand(dist, d)
+  z = scale*u + location
 ```
-
 """
-struct VILocationScale{L, S, D, R} <: ContinuousMultivariateDistribution
+struct VILocationScale{L, S, D} <: ContinuousMultivariateDistribution
     location::L
     scale   ::S
     dist    ::D
-    epsilon ::R
 
     function VILocationScale(μ::AbstractVector{<:Real},
                              L::Union{<:AbstractTriangular{<:Real},
                                       <:Diagonal{<:Real}},
-                             q_base::ContinuousUnivariateDistribution,
-                             epsilon::Real)
+                             q_base::ContinuousUnivariateDistribution)
         # Restricting all the arguments to have the same types creates problems 
         # with dual-variable-based AD frameworks.
         @assert (length(μ) == size(L,1)) && (length(μ) == size(L,2))
-        new{typeof(μ), typeof(L), typeof(q_base), typeof(epsilon)}(μ, L, q_base, epsilon)
+        new{typeof(μ), typeof(L), typeof(q_base)}(μ, L, q_base)
     end
 end
 
@@ -76,16 +76,22 @@ function _rand!(rng::AbstractRNG, q::VILocationScale, x::AbstractMatrix{<:Real})
     return x += location
 end
 
-function VIFullRankGaussian(μ::AbstractVector{T},
-                            L::AbstractTriangular{T},
-                            epsilon::Real = eps(T)) where {T <: Real}
+"""
+    VIFullRankGaussian(μ::AbstractVector{T}, L::AbstractTriangular{T})
+
+This constructs a multivariate Gaussian distribution with a full rank covariance matrix.
+"""
+function VIFullRankGaussian(μ::AbstractVector{T}, L::AbstractTriangular{T}) where {T <: Real}
     q_base = Normal{T}(zero(T), one(T))
-    VILocationScale(μ, L, q_base, epsilon)
+    VILocationScale(μ, L, q_base)
 end
 
-function VIMeanFieldGaussian(μ::AbstractVector{T},
-                             L::Diagonal{T},
-                             epsilon::Real = eps(T)) where {T <: Real}
+"""
+    VIFullRankGaussian(μ::AbstractVector{T}, L::AbstractTriangular{T})
+
+This constructs a multivariate Gaussian distribution with a diagonal covariance matrix.
+"""
+function VIMeanFieldGaussian(μ::AbstractVector{T}, L::Diagonal{T}) where {T <: Real}
     q_base = Normal{T}(zero(T), one(T))
-    VILocationScale(μ, L, q_base, epsilon)
+    VILocationScale(μ, L, q_base)
 end
