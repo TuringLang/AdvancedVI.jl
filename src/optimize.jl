d5026e14aa441c32b12fa7050c583ec0011d0063
Kyurae Kim <msca8h@naver.com>
2023-10-23 00:41:19 -0400

    fix simplify description for `optimize`
    Co-authored-by: Tor Erlend Fjelde <tor.github@gmail.com>

--- src/optimize.jl
+++ src/optimize.jl
@@ -18,7 +18,9 @@
         kwargs...
     )              
 
-Optimize the variational objective `objective` targeting the problem `problem` by estimating (stochastic) gradients, where the variational approximation can be constructed by passing the variational parameters `param_init` or the initial variational approximation `variational_dist_init` to the function `restructure`.
+Optimize the variational objective `objective` targeting the problem `problem` by estimating (stochastic) gradients.
+
+The variational approximation can be constructed by passing the variational parameters `param_init` or the initial variational approximation `variational_dist_init` to the function `restructure`.
 
 # Arguments
 - `objective`: Variational Objective.
