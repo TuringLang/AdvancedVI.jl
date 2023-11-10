65e9b126a6c0038bac9f6904e8d851b211b528b3
Kyurae Kim <kyrkim@seas.upenn.edu>
2023-11-10 03:25:15 -0500

    add inference test for a non-bijector model

--- test/inference/advi_distributionsad.jl
+++ test/inference/advi_distributionsad.jl
@@ -8,6 +8,81 @@ using Test
         @testset "$(modelname) $(objname) $(realtype) $(adbackname)"  for
             realtype ∈ [Float64], # Currently only tested against Float64
             (modelname, modelconstr) ∈ Dict(
+                :Normal=> normal_meanfield,
+            ),
+            (objname, objective) ∈ Dict(
+                :ADVIClosedFormEntropy  => ADVI(10),
+                :ADVIStickingTheLanding => ADVI(10, entropy = StickingTheLandingEntropy()),
+            ),
+            (adbackname, adbackend) ∈ Dict(
+                :ForwarDiff  => AutoForwardDiff(),
+                #:ReverseDiff => AutoReverseDiff(),
+                #:Zygote      => AutoZygote(), 
+                #:Enzyme      => AutoEnzyme(),
+            )
+
+            seed = (0x38bef07cf9cc549d)
+            rng  = StableRNG(seed)
+
+            modelstats = modelconstr(rng, realtype)
+            @unpack model, μ_true, L_true, n_dims, is_meanfield = modelstats
+
+            T, η = is_meanfield ? (5_000, 1e-2) : (30_000, 1e-3)
+
+            μ₀   = Zeros(realtype, n_dims)
+            L₀   = Diagonal(Ones(realtype, n_dims))
+            q₀_z = TuringDiagMvNormal(μ₀, diag(L₀))
+
+            @testset "convergence" begin
+                Δλ₀ = sum(abs2, μ₀ - μ_true) + sum(abs2, L₀ - L_true)
+                q, stats, _ = optimize(
+                    rng, model, objective, q₀_z, T;
+                    optimizer     = Optimisers.Adam(realtype(η)),
+                    show_progress = PROGRESS,
+                    adbackend     = adbackend,
+                )
+
+                μ  = mean(q)
+                L  = sqrt(cov(q))
+                Δλ = sum(abs2, μ - μ_true) + sum(abs2, L - L_true)
+
+                @test Δλ ≤ Δλ₀/T^(1/4)
+                @test eltype(μ) == eltype(μ_true)
+                @test eltype(L) == eltype(L_true)
+            end
+
+            @testset "determinism" begin
+                rng = StableRNG(seed)
+                q, stats, _ = optimize(
+                    rng, model, objective, q₀_z, T;
+                    optimizer     = Optimisers.Adam(realtype(η)),
+                    show_progress = PROGRESS,
+                    adbackend     = adbackend,
+                )
+                μ  = mean(q)
+                L  = sqrt(cov(q))
+
+                rng_repl = StableRNG(seed)
+                q, stats, _ = optimize(
+                    rng_repl, model, objective, q₀_z, T;
+                    optimizer     = Optimisers.Adam(realtype(η)),
+                    show_progress = PROGRESS,
+                    adbackend     = adbackend,
+                )
+                μ_repl = mean(q)
+                L_repl = sqrt(cov(q))
+                @test μ == μ_repl
+                @test L == L_repl
+            end
+        end
+    end
+end
+
+@testset "inference_bijectors_advi" begin
+    @testset "distributionsad" begin
+        @testset "$(modelname) $(objname) $(realtype) $(adbackname)"  for
+            realtype ∈ [Float64], # Currently only tested against Float64
+            (modelname, modelconstr) ∈ Dict(
                 :NormalLogNormalMeanField => normallognormal_meanfield,
             ),
             (objname, objective) ∈ Dict(
@@ -16,9 +91,9 @@ using Test
             ),
             (adbackname, adbackend) ∈ Dict(
                 :ForwarDiff  => AutoForwardDiff(),
-                # :ReverseDiff => AutoReverseDiff(),
-                # :Zygote      => AutoZygote(), 
-                # :Enzyme      => AutoEnzyme(),
+                #:ReverseDiff => AutoReverseDiff(),
+                #:Zygote      => AutoZygote(), 
+                #:Enzyme      => AutoEnzyme(),
             )
 
             seed = (0x38bef07cf9cc549d)
