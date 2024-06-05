314eacf763af1c52153759268e30e87fbf17c5e7
Kyurae Kim <msca8h@naver.com>
2024-06-05 19:34:12 -0400

    add continuous benchmarking (#61)

---
+++ bench/normallognormal.jl
@@ -0,0 +1,56 @@
+
+struct NormalLogNormal{MX,SX,MY,SY}
+    μ_x::MX
+    σ_x::SX
+    μ_y::MY
+    Σ_y::SY
+end
+
+function LogDensityProblems.logdensity(model::NormalLogNormal, θ)
+    (; μ_x, σ_x, μ_y, Σ_y) = model
+    logpdf(LogNormal(μ_x, σ_x), θ[1]) + logpdf(MvNormal(μ_y, Σ_y), θ[2:end])
+end
+
+function LogDensityProblems.dimension(model::NormalLogNormal)
+    length(model.μ_y) + 1
+end
+
+function LogDensityProblems.capabilities(::Type{<:NormalLogNormal})
+    LogDensityProblems.LogDensityOrder{0}()
+end
+
+function Bijectors.bijector(model::NormalLogNormal)
+    (; μ_x, σ_x, μ_y, Σ_y) = model
+    Bijectors.Stacked(
+        Bijectors.bijector.([LogNormal(μ_x, σ_x), MvNormal(μ_y, Σ_y)]),
+        [1:1, 2:1+length(μ_y)])
+end
+
+function normallognormal(; fptype, adtype, family, objective, kwargs...)
+    n_dims = 10
+    μ_x    = fptype(5.0)
+    σ_x    = fptype(0.3)
+    μ_y    = Fill(fptype(5.0), n_dims)
+    σ_y    = Fill(fptype(0.3), n_dims)
+    model  = NormalLogNormal(μ_x, σ_x, μ_y, Diagonal(σ_y.^2))
+
+    obj = variational_objective(objective; kwargs...)
+
+    d = LogDensityProblems.dimension(model)
+    q = variational_standard_mvnormal(fptype, d, family) 
+
+    b             = Bijectors.bijector(model)
+    binv          = inverse(b)
+    q_transformed = Bijectors.TransformedDistribution(q, binv)
+
+    max_iter = 10^3
+    AdvancedVI.optimize(
+        model,
+        obj,
+        q_transformed,
+        max_iter;
+        adtype,
+        optimizer = Optimisers.Adam(fptype(1e-3)),
+        show_progress = false,
+    )
+end
---
+++ bench/utils.jl
@@ -0,0 +1,20 @@
+
+function variational_standard_mvnormal(type::Type, n_dims::Int, family::Symbol)
+    if family == :meanfield
+        AdvancedVI.MeanFieldGaussian(
+            zeros(type, n_dims), Diagonal(ones(type, n_dims))
+        )
+    else
+        AdvancedVI.FullRankGaussian(
+            zeros(type, n_dims), Matrix(type, I, n_dims, n_dims)
+        )
+    end
+end
+
+function variational_objective(objective::Symbol; kwargs...)
+    if objective == :RepGradELBO
+        AdvancedVI.RepGradELBO(kwargs[:n_montecarlo])
+    elseif objective == :RepGradELBOSTL
+        AdvancedVI.RepGradELBO(kwargs[:n_montecarlo], entropy=StickingTheLandingEntropy())
+    end
+end
