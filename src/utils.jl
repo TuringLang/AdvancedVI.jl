9062431dad05a819a9c105ad7ba826d142f34d08
Ray Kim <msca8h@naver.com>
2024-06-13 03:32:42 +0100

    fix use words instead of greek letters

--- src/utils.jl
+++ src/utils.jl
@@ -6,19 +6,19 @@ end
 function maybe_init_optimizer(
     state_init::NamedTuple,
     optimizer ::Optimisers.AbstractRule,
-    λ         ::AbstractVector
+    params    ::AbstractVector
 )
-    haskey(state_init, :optimizer) ? state_init.optimizer : Optimisers.setup(optimizer, λ)
+    haskey(state_init, :optimizer) ? state_init.optimizer : Optimisers.setup(optimizer, params)
 end
 
 function maybe_init_objective(
     state_init::NamedTuple,
     rng       ::Random.AbstractRNG,
     objective ::AbstractVariationalObjective,
-    λ         ::AbstractVector,
+    params    ::AbstractVector,
     restructure
 )
-    haskey(state_init, :objective) ? state_init.objective : init(rng, objective, λ, restructure)
+    haskey(state_init, :objective) ? state_init.objective : init(rng, objective, params, restructure)
 end
 
 eachsample(samples::AbstractMatrix) = eachcol(samples)
