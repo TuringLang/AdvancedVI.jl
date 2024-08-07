0f547cd3d6f0384cb9ecee78776cedde0a549970
David Widmann <devmotion@users.noreply.github.com>
2024-08-07 09:18:26 +0200

    Fix format

--- test/interface/ad.jl
+++ test/interface/ad.jl
@@ -3,11 +3,11 @@ using Test
 
 @testset "ad" begin
     @testset "$(adname)" for (adname, adsymbol) ∈ Dict(
-          :ForwardDiff => AutoForwardDiff(),
-          :ReverseDiff => AutoReverseDiff(),
-          :Zygote => AutoZygote(),
-          :Enzyme => AutoEnzyme(),
-        )
+        :ForwardDiff => AutoForwardDiff(),
+        :ReverseDiff => AutoReverseDiff(),
+        :Zygote => AutoZygote(),
+        :Enzyme => AutoEnzyme(),
+    )
         D = 10
         A = randn(D, D)
         λ = randn(D)
