e9e48a521c2c8efaad26011c49a911a6cd612ffc
David Widmann <devmotion@users.noreply.github.com>
2024-08-07 09:01:49 +0200

    Enable Enzyme tests

--- test/interface/ad.jl
+++ test/interface/ad.jl
@@ -6,7 +6,7 @@ using Test
           :ForwardDiff => AutoForwardDiff(),
           :ReverseDiff => AutoReverseDiff(),
           :Zygote      => AutoZygote(),
-          # :Enzyme      => AutoEnzyme() # Currently not tested against
+          :Enzyme      => AutoEnzyme(),
         )
         D = 10
         A = randn(D, D)
