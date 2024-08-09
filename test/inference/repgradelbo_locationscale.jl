b2c7fa03c12023b3014a13233ca29f7d686e503d
Hong Ge <hg344@cam.ac.uk>
2024-08-09 15:48:37 +0100

    formatting all files

--- test/inference/repgradelbo_locationscale.jl
+++ test/inference/repgradelbo_locationscale.jl
@@ -1,59 +1,62 @@
 
 @testset "inference RepGradELBO VILocationScale" begin
-    @testset "$(modelname) $(objname) $(realtype) $(adbackname)"  for
-        realtype in [Float64, Float32],
-        (modelname, modelconstr) in Dict(
-            :Normal=> normal_meanfield,
-            :Normal=> normal_fullrank,
-        ),
+    @testset "$(modelname) $(objname) $(realtype) $(adbackname)" for realtype in
+                                                                     [Float64, Float32],
+        (modelname, modelconstr) in
+        Dict(:Normal => normal_meanfield, :Normal => normal_fullrank),
         n_montecarlo in [1, 10],
         (objname, objective) in Dict(
-            :RepGradELBOClosedFormEntropy  => RepGradELBO(n_montecarlo),
-            :RepGradELBOStickingTheLanding => RepGradELBO(n_montecarlo, entropy = StickingTheLandingEntropy()),
+            :RepGradELBOClosedFormEntropy => RepGradELBO(n_montecarlo),
+            :RepGradELBOStickingTheLanding =>
+                RepGradELBO(n_montecarlo; entropy=StickingTheLandingEntropy()),
         ),
         (adbackname, adtype) in Dict(
-            :ForwarDiff  => AutoForwardDiff(),
+            :ForwarDiff => AutoForwardDiff(),
             :ReverseDiff => AutoReverseDiff(),
-            :Zygote      => AutoZygote(), 
+            :Zygote => AutoZygote(),
             #:Enzyme      => AutoEnzyme(),
         )
 
         seed = (0x38bef07cf9cc549d)
-        rng  = StableRNG(seed)
+        rng = StableRNG(seed)
 
         modelstats = modelconstr(rng, realtype)
         @unpack model, μ_true, L_true, n_dims, strong_convexity, is_meanfield = modelstats
 
-        T   = 1000
-        η   = 1e-3
+        T = 1000
+        η = 1e-3
         opt = Optimisers.Descent(realtype(η))
 
         # For small enough η, the error of SGD, Δλ, is bounded as
         #     Δλ ≤ ρ^T Δλ0 + O(η),
         # where ρ = 1 - ημ, μ is the strong convexity constant.
-        contraction_rate = 1 - η*strong_convexity
+        contraction_rate = 1 - η * strong_convexity
 
         q0 = if is_meanfield
             MeanFieldGaussian(zeros(realtype, n_dims), Diagonal(ones(realtype, n_dims)))
         else
-            L0 = Matrix{realtype}(I, n_dims, n_dims) |> LowerTriangular
+            L0 = LowerTriangular(Matrix{realtype}(I, n_dims, n_dims))
             FullRankGaussian(zeros(realtype, n_dims), L0)
         end
 
         @testset "convergence" begin
             Δλ0 = sum(abs2, q0.location - μ_true) + sum(abs2, q0.scale - L_true)
             q, stats, _ = optimize(
-                rng, model, objective, q0, T;
-                optimizer     = opt,
-                show_progress = PROGRESS,
-                adtype        = adtype,
+                rng,
+                model,
+                objective,
+                q0,
+                T;
+                optimizer=opt,
+                show_progress=PROGRESS,
+                adtype=adtype,
             )
 
-            μ  = q.location
-            L  = q.scale
+            μ = q.location
+            L = q.scale
             Δλ = sum(abs2, μ - μ_true) + sum(abs2, L - L_true)
 
-            @test Δλ ≤ contraction_rate^(T/2)*Δλ0
+            @test Δλ ≤ contraction_rate^(T / 2) * Δλ0
             @test eltype(μ) == eltype(μ_true)
             @test eltype(L) == eltype(L_true)
         end
@@ -61,20 +64,28 @@
         @testset "determinism" begin
             rng = StableRNG(seed)
             q, stats, _ = optimize(
-                rng, model, objective, q0, T;
-                optimizer     = opt,
-                show_progress = PROGRESS,
-                adtype        = adtype,
+                rng,
+                model,
+                objective,
+                q0,
+                T;
+                optimizer=opt,
+                show_progress=PROGRESS,
+                adtype=adtype,
             )
-            μ  = q.location
-            L  = q.scale
+            μ = q.location
+            L = q.scale
 
             rng_repl = StableRNG(seed)
             q, stats, _ = optimize(
-                rng_repl, model, objective, q0, T;
-                optimizer     = opt,
-                show_progress = PROGRESS,
-                adtype        = adtype,
+                rng_repl,
+                model,
+                objective,
+                q0,
+                T;
+                optimizer=opt,
+                show_progress=PROGRESS,
+                adtype=adtype,
             )
             μ_repl = q.location
             L_repl = q.scale
@@ -83,4 +94,3 @@
         end
     end
 end
-
--- test/interface/repgradelbo.jl
+++ test/interface/repgradelbo.jl
@@ -3,7 +3,7 @@ using Test
 
 @testset "interface RepGradELBO" begin
     seed = (0x38bef07cf9cc549d)
-    rng  = StableRNG(seed)
+    rng = StableRNG(seed)
 
     modelstats = normal_meanfield(rng, Float64)
 
@@ -11,37 +11,34 @@ using Test
 
     q0 = TuringDiagMvNormal(zeros(Float64, n_dims), ones(Float64, n_dims))
 
-    obj      = RepGradELBO(10)
-    rng      = StableRNG(seed)
+    obj = RepGradELBO(10)
+    rng = StableRNG(seed)
     elbo_ref = estimate_objective(rng, obj, q0, model; n_samples=10^4)
 
     @testset "determinism" begin
-        rng  = StableRNG(seed)
+        rng = StableRNG(seed)
         elbo = estimate_objective(rng, obj, q0, model; n_samples=10^4)
         @test elbo == elbo_ref
     end
 
     @testset "default_rng" begin
         elbo = estimate_objective(obj, q0, model; n_samples=10^4)
-        @test elbo ≈ elbo_ref rtol=0.1
+        @test elbo ≈ elbo_ref rtol = 0.1
     end
 end
 
 @testset "interface RepGradELBO STL variance reduction" begin
     seed = (0x38bef07cf9cc549d)
-    rng  = StableRNG(seed)
+    rng = StableRNG(seed)
 
     modelstats = normal_meanfield(rng, Float64)
     @unpack model, μ_true, L_true, n_dims, is_meanfield = modelstats
 
     @testset for ad in [
-        ADTypes.AutoForwardDiff(),
-        ADTypes.AutoReverseDiff(),
-        ADTypes.AutoZygote()
+        ADTypes.AutoForwardDiff(), ADTypes.AutoReverseDiff(), ADTypes.AutoZygote()
     ]
         q_true = MeanFieldGaussian(
-            Vector{eltype(μ_true)}(μ_true),
-            Diagonal(Vector{eltype(L_true)}(diag(L_true)))
+            Vector{eltype(μ_true)}(μ_true), Diagonal(Vector{eltype(L_true)}(diag(L_true)))
         )
         params, re = Optimisers.destructure(q_true)
         obj = RepGradELBO(10; entropy=StickingTheLandingEntropy())
@@ -52,6 +49,6 @@ end
             ad, AdvancedVI.estimate_repgradelbo_ad_forward, params, aux, out
         )
         grad = DiffResults.gradient(out)
-        @test norm(grad) ≈ 0 atol=1e-5
+        @test norm(grad) ≈ 0 atol = 1e-5
     end
 end
