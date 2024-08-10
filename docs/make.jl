96eae86df0128105907caaa797d4d13a4e292cb5
Kyurae Kim <kyrkim@seas.upenn.edu>
2024-08-10 02:44:10 -0400

    run formatter

--- docs/make.jl
+++ docs/make.jl
@@ -2,23 +2,23 @@
 using AdvancedVI
 using Documenter
 
-DocMeta.setdocmeta!(
-    AdvancedVI, :DocTestSetup, :(using AdvancedVI); recursive=true
-)
+DocMeta.setdocmeta!(AdvancedVI, :DocTestSetup, :(using AdvancedVI); recursive=true)
 
 makedocs(;
-    modules  = [AdvancedVI],
-    sitename = "AdvancedVI.jl",
-    repo     = "https://github.com/TuringLang/AdvancedVI.jl/blob/{commit}{path}#{line}",
-    format   = Documenter.HTML(; prettyurls = get(ENV, "CI", nothing) == "true"),
-    pages    = ["AdvancedVI"        => "index.md",
-                "General Usage"     => "general.md",
-                "Examples"          => "examples.md",
-                "ELBO Maximization" => [
-                    "Overview"                               => "elbo/overview.md",   
-                    "Reparameterization Gradient Estimator"  => "elbo/repgradelbo.md",   
-                    "Variational Families"                   => "elbo/families.md",
-                ]],
+    modules=[AdvancedVI],
+    sitename="AdvancedVI.jl",
+    repo="https://github.com/TuringLang/AdvancedVI.jl/blob/{commit}{path}#{line}",
+    format=Documenter.HTML(; prettyurls=get(ENV, "CI", nothing) == "true"),
+    pages=[
+        "AdvancedVI" => "index.md",
+        "General Usage" => "general.md",
+        "Examples" => "examples.md",
+        "ELBO Maximization" => [
+            "Overview" => "elbo/overview.md",
+            "Reparameterization Gradient Estimator" => "elbo/repgradelbo.md",
+            "Variational Families" => "elbo/families.md",
+        ],
+    ],
 )
 
 deploydocs(; repo="github.com/TuringLang/AdvancedVI.jl", push_preview=true)
--- test/families/location_scale_low_rank.jl
+++ test/families/location_scale_low_rank.jl
@@ -1,17 +1,16 @@
 
 @testset "interface LocationScaleLowRank" begin
-    @testset "$(basedist) rank=$(rank) $(realtype)" for
-        basedist in [:gaussian],
-        rank     in [1, 2],
+    @testset "$(basedist) rank=$(rank) $(realtype)" for basedist in [:gaussian],
+        rank in [1, 2],
         realtype in [Float32, Float64]
 
-        n_dims       = 10
+        n_dims = 10
         n_montecarlo = 1000_000
 
         μ = randn(realtype, n_dims)
         D = ones(realtype, n_dims)
         U = randn(realtype, n_dims, rank)
-        Σ = Diagonal(D.^2) + U*U'
+        Σ = Diagonal(D .^ 2) + U * U'
 
         q = if basedist == :gaussian
             LowRankGaussian(μ, D, U)
@@ -26,13 +25,13 @@
 
         @testset "logpdf" begin
             z = rand(q)
-            @test logpdf(q, z)          ≈  logpdf(q_true, z)  rtol=realtype(1e-2)
-            @test eltype(logpdf(q, z))  == realtype 
+            @test logpdf(q, z) ≈ logpdf(q_true, z) rtol = realtype(1e-2)
+            @test eltype(logpdf(q, z)) == realtype
         end
 
         @testset "entropy" begin
             @test eltype(entropy(q)) == realtype
-            @test entropy(q)         ≈ entropy(q_true)
+            @test entropy(q) ≈ entropy(q_true)
         end
 
         @testset "length" begin
@@ -41,37 +40,41 @@
 
         @testset "statistics" begin
             @testset "mean" begin
-                 @test eltype(mean(q)) == realtype
-                 @test mean(q)         == μ
+                @test eltype(mean(q)) == realtype
+                @test mean(q) == μ
             end
             @testset "var" begin
-                 @test eltype(var(q)) == realtype
-		 @test var(q)         ≈  Diagonal(Σ)
+                @test eltype(var(q)) == realtype
+                @test var(q) ≈ Diagonal(Σ)
             end
             @testset "cov" begin
-                 @test eltype(cov(q)) == realtype
-		 @test cov(q)         ≈  Σ
+                @test eltype(cov(q)) == realtype
+                @test cov(q) ≈ Σ
             end
         end
 
         @testset "sampling" begin
             @testset "rand" begin
-                z_samples  = mapreduce(x -> rand(q), hcat, 1:n_montecarlo)
+                z_samples = mapreduce(x -> rand(q), hcat, 1:n_montecarlo)
                 @test eltype(z_samples) == realtype
-                @test dropdims(mean(z_samples, dims=2), dims=2) ≈ μ       rtol=realtype(1e-2)
-                @test dropdims(var(z_samples, dims=2),  dims=2) ≈ diag(Σ) rtol=realtype(1e-2)
-                @test cov(z_samples, dims=2)                    ≈ Σ       rtol=realtype(1e-2)
+                @test dropdims(mean(z_samples; dims=2); dims=2) ≈ μ rtol = realtype(1e-2)
+                @test dropdims(var(z_samples; dims=2); dims=2) ≈ diag(Σ) rtol = realtype(
+                    1e-2
+                )
+                @test cov(z_samples; dims=2) ≈ Σ rtol = realtype(1e-2)
 
                 z_sample_ref = rand(StableRNG(1), q)
                 @test z_sample_ref == rand(StableRNG(1), q)
             end
 
             @testset "rand batch" begin
-                z_samples  = rand(q, n_montecarlo)
+                z_samples = rand(q, n_montecarlo)
                 @test eltype(z_samples) == realtype
-                @test dropdims(mean(z_samples, dims=2), dims=2) ≈ μ       rtol=realtype(1e-2)
-                @test dropdims(var(z_samples, dims=2),  dims=2) ≈ diag(Σ) rtol=realtype(1e-2)
-                @test cov(z_samples, dims=2)                    ≈ Σ       rtol=realtype(1e-2)
+                @test dropdims(mean(z_samples; dims=2); dims=2) ≈ μ rtol = realtype(1e-2)
+                @test dropdims(var(z_samples; dims=2); dims=2) ≈ diag(Σ) rtol = realtype(
+                    1e-2
+                )
+                @test cov(z_samples; dims=2) ≈ Σ rtol = realtype(1e-2)
 
                 samples_ref = rand(StableRNG(1), q, n_montecarlo)
                 @test samples_ref == rand(StableRNG(1), q, n_montecarlo)
@@ -79,16 +82,18 @@
 
             @testset "rand! AbstractVector" begin
                 res = map(1:n_montecarlo) do _
-                    z_sample     = Array{realtype}(undef, n_dims)
+                    z_sample = Array{realtype}(undef, n_dims)
                     z_sample_ret = rand!(q, z_sample)
                     (z_sample, z_sample_ret)
                 end
-                z_samples     = mapreduce(first, hcat, res)
+                z_samples = mapreduce(first, hcat, res)
                 z_samples_ret = mapreduce(last, hcat, res)
                 @test z_samples == z_samples_ret
-                @test dropdims(mean(z_samples, dims=2), dims=2) ≈ μ       rtol=realtype(1e-2)
-                @test dropdims(var(z_samples, dims=2),  dims=2) ≈ diag(Σ) rtol=realtype(1e-2)
-                @test cov(z_samples, dims=2)                    ≈ Σ       rtol=realtype(1e-2)
+                @test dropdims(mean(z_samples; dims=2); dims=2) ≈ μ rtol = realtype(1e-2)
+                @test dropdims(var(z_samples; dims=2); dims=2) ≈ diag(Σ) rtol = realtype(
+                    1e-2
+                )
+                @test cov(z_samples; dims=2) ≈ Σ rtol = realtype(1e-2)
 
                 z_sample_ref = Array{realtype}(undef, n_dims)
                 rand!(StableRNG(1), q, z_sample_ref)
@@ -99,12 +104,14 @@
             end
 
             @testset "rand! AbstractMatrix" begin
-                z_samples     = Array{realtype}(undef, n_dims, n_montecarlo)
+                z_samples = Array{realtype}(undef, n_dims, n_montecarlo)
                 z_samples_ret = rand!(q, z_samples)
                 @test z_samples == z_samples_ret
-                @test dropdims(mean(z_samples, dims=2), dims=2) ≈ μ       rtol=realtype(1e-2)
-                @test dropdims(var(z_samples, dims=2),  dims=2) ≈ diag(Σ) rtol=realtype(1e-2)
-                @test cov(z_samples, dims=2)                    ≈ Σ       rtol=realtype(1e-2)
+                @test dropdims(mean(z_samples; dims=2); dims=2) ≈ μ rtol = realtype(1e-2)
+                @test dropdims(var(z_samples; dims=2); dims=2) ≈ diag(Σ) rtol = realtype(
+                    1e-2
+                )
+                @test cov(z_samples; dims=2) ≈ Σ rtol = realtype(1e-2)
 
                 z_samples_ref = Array{realtype}(undef, n_dims, n_montecarlo)
                 rand!(StableRNG(1), q, z_samples_ref)
@@ -117,9 +124,8 @@
     end
 
     @testset "diagonal positive definite projection" begin
-        @testset "$(realtype) $(bijector)" for
-            realtype    = [Float32,     Float64],
-            bijector    = [nothing,    :identity]
+        @testset "$(realtype) $(bijector)" for realtype in [Float32, Float64],
+            bijector in [nothing, :identity]
 
             rank = 2
             d = 5
@@ -130,18 +136,18 @@
             q = MvLocationScaleLowRank(
                 μ, D, U, Normal{realtype}(zero(realtype), one(realtype)); scale_eps=ϵ
             )
-            q_trans = if isnothing(bijector) 
+            q_trans = if isnothing(bijector)
                 q
             else
                 Bijectors.TransformedDistribution(q, bijector)
             end
             g = deepcopy(q)
 
-            λ, re   = Optimisers.destructure(q)
+            λ, re = Optimisers.destructure(q)
             grad, _ = Optimisers.destructure(g)
-            opt_st  = Optimisers.setup(Descent(one(realtype)), λ)
-            _, λ′    = AdvancedVI.update_variational_params!(typeof(q), opt_st, λ, re, grad)
-            q′       = re(λ′)
+            opt_st = Optimisers.setup(Descent(one(realtype)), λ)
+            _, λ′ = AdvancedVI.update_variational_params!(typeof(q), opt_st, λ, re, grad)
+            q′ = re(λ′)
             @test all(diag(var(q′)) .≥ ϵ^2)
         end
     end
