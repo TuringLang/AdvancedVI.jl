0ff9e3bb8e68ff0dca1fcf6ea6e4b81df327677a
github-action-benchmark <github@users.noreply.github.com>
2024-09-10 12:55:43 +0000

    add Benchmark Results (julia) benchmark result for 4b08a6051bb5a3057730f7dc5c9cc524332df11b

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1725972108795,
+  "lastUpdate": 1725972943512,
   "repoUrl": "https://github.com/TuringLang/AdvancedVI.jl",
   "entries": {
     "Benchmark Results": [
@@ -96,6 +96,38 @@ window.BENCHMARK_DATA = {
             "value": 181722821.5,
             "unit": "ns",
             "extra": "gctime=8642751\nmemory=98515216\nallocs=2029783\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "TuringLang",
+            "username": "TuringLang"
+          },
+          "committer": {
+            "name": "TuringLang",
+            "username": "TuringLang"
+          },
+          "id": "4b08a6051bb5a3057730f7dc5c9cc524332df11b",
+          "message": "Only upload benchmark results if not on a fork",
+          "timestamp": "2024-09-10T11:11:17Z",
+          "url": "https://github.com/TuringLang/AdvancedVI.jl/pull/91/commits/4b08a6051bb5a3057730f7dc5c9cc524332df11b"
+        },
+        "date": 1725972940420,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "normal + bijector/meanfield/ForwardDiff",
+            "value": 500610560,
+            "unit": "ns",
+            "extra": "gctime=30873436\nmemory=816851152\nallocs=2729782\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          },
+          {
+            "name": "normal + bijector/meanfield/ReverseDiff",
+            "value": 175928090,
+            "unit": "ns",
+            "extra": "gctime=8818597\nmemory=98515216\nallocs=2029783\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
           }
         ]
       }
