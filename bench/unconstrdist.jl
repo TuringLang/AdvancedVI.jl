02a831cd213f50b45b311cc5ccbd1a6a19f72dbc
Kyurae Kim <kyrkim@seas.upenn.edu>
2024-10-03 22:31:22 -0700

    remove unused code

--- bench/unconstrdist.jl
+++ bench/unconstrdist.jl
@@ -1,6 +1,4 @@
 
-using Distributions, Enzyme, DifferentiationInterface, LinearAlgebra, LogDensityProblems
-
 struct UnconstrDist{D <: ContinuousMultivariateDistribution}
     dist::D
 end
@@ -25,30 +23,4 @@ function normal(; n_dims=10, fptype=Float64)
     μ = fill(fptype(5), n_dims)
     Σ = Diagonal(ones(fptype, n_dims))
     UnconstrDist(MvNormal(μ, Σ))
-end
-
-function f(x, aux)
-    LogDensityProblems.logdensity(aux , x)
-end
-
-function main()
-    n_dims = 10
-    x      = randn(10)
-
-    for fptype in [Float32, Float64],
-        aux in [
-            UnconstrDist(MvNormal(fill(fptype(5), n_dims), Diagonal(ones(fptype, n_dims)))),
-            UnconstrDist(MvNormal(fill(fptype(5), n_dims), ones(fptype, n_dims))),
-            UnconstrDist(MvNormal(fill(fptype(5), n_dims), I)),
-        ]
-        ∇x = zeros(n_dims)
-        _, y = Enzyme.autodiff(
-            Enzyme.set_runtime_activity(Enzyme.ReverseWithPrimal, true),
-            Enzyme.Const(f),
-            Enzyme.Active,
-            Enzyme.Duplicated(x, ∇x),
-            Enzyme.Const(aux),
-        )
-        println(y)
-    end
 end
