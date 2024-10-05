85eec20cef91fbe52f28105e37ac6bfd5de8c16d
github-action-benchmark <github@users.noreply.github.com>
2024-10-05 00:23:35 +0000

    add Benchmark Results (julia) benchmark result for e47f1593b5793fb760e8cf67e1e1412456adcf20

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1728087808133,
+  "lastUpdate": 1728087815727,
   "repoUrl": "https://github.com/TuringLang/AdvancedVI.jl",
   "entries": {
     "Benchmark Results": [
@@ -1256,6 +1256,38 @@ window.BENCHMARK_DATA = {
             "value": 194399549,
             "unit": "ns",
             "extra": "gctime=9014274\nmemory=112803088\nallocs=2256783\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "TuringLang",
+            "username": "TuringLang"
+          },
+          "committer": {
+            "name": "TuringLang",
+            "username": "TuringLang"
+          },
+          "id": "e47f1593b5793fb760e8cf67e1e1412456adcf20",
+          "message": "CompatHelper: add new compat entry for Zygote at version 0.6 for package bench, (keep existing compat)",
+          "timestamp": "2024-10-04T17:20:23Z",
+          "url": "https://github.com/TuringLang/AdvancedVI.jl/pull/110/commits/e47f1593b5793fb760e8cf67e1e1412456adcf20"
+        },
+        "date": 1728087812086,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "normal + bijector/meanfield/ForwardDiff",
+            "value": 463412578,
+            "unit": "ns",
+            "extra": "gctime=23280887\nmemory=817171024\nallocs=2732782\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
+          },
+          {
+            "name": "normal + bijector/meanfield/ReverseDiff",
+            "value": 192181496.5,
+            "unit": "ns",
+            "extra": "gctime=8404075.5\nmemory=112803088\nallocs=2256783\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":5,\"overhead\":0,\"memory_tolerance\":0.01}"
           }
         ]
       }
