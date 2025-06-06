7c53e63dfe9fe0729095d451c266ddd9be54ae61
Kyurae Kim <kyrkim@seas.upenn.edu>
2025-06-06 14:31:29 -0400

    refactor move elbo-specific exports and interfaces to elbo.jl

--- src/AdvancedVI.jl
+++ src/AdvancedVI.jl
@@ -94,38 +94,8 @@ This is an indirection for handling the type stability of `restructure`, as some
 """
 restructure_ad_forward(::ADTypes.AbstractADType, restructure, params) = restructure(params)
 
-
-# ELBO-specific interfaces
-abstract type AbstractEntropyEstimator end
-
-"""
-    estimate_entropy(entropy_estimator, mc_samples, q, q_stop)
-
-Estimate the entropy of `q`.
-
-# Arguments
-- `entropy_estimator`: Entropy estimation strategy.
-- `q`: Variational approximation.
-- `q_stop`: Variational approximation with detached from the automatic differentiation graph.
-- `mc_samples`: Monte Carlo samples used to estimate the entropy. (Only used for Monte Carlo strategies.)
-
-# Returns
-- `obj_est`: Estimate of the objective value.
-"""
-function estimate_entropy end
-
-export RepGradELBO,
-    ScoreGradELBO,
-    ClosedFormEntropy,
-    StickingTheLandingEntropy,
-    MonteCarloEntropy,
-    ClosedFormEntropyZeroGradient,
-    StickingTheLandingEntropyZeroGradient
-
 include("algorithms/paramspacesgd/interface.jl")
-include("algorithms/paramspacesgd/entropy.jl")
-include("algorithms/paramspacesgd/repgradelbo.jl")
-include("algorithms/paramspacesgd/scoregradelbo.jl")
+include("algorithms/paramspacesgd/elbo/elbo.jl")
 
 # Variational Families
 export MvLocationScale, MeanFieldGaussian, FullRankGaussian
