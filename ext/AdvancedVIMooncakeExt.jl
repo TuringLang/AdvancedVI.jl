070dc202ca55b1b182b659500887309671df0f9b
Kyurae Kim <kyrkim@seas.upenn.edu>
2025-07-12 01:57:48 -0400

    fix MooncakeExt

--- ext/AdvancedVIMooncakeExt.jl
+++ ext/AdvancedVIMooncakeExt.jl
@@ -1,11 +1,17 @@
 module AdvancedVIMooncakeExt
 
 using AdvancedVI
+using Base: IEEEFloat
 using LogDensityProblems
 using Mooncake
 
+# Array types explicitly tested by Mooncake
+const SupportedArray{P,N} = Union{Array{P,N},AbstractGPUArray{P,N}}
+
 Mooncake.@from_rrule Mooncake.DefaultCtx Tuple{
-    typeof(LogDensityProblems.logdensity),AdvancedVI.MixedADLogDensityProblem,AbstractVector
+    typeof(LogDensityProblems.logdensity),
+    AdvancedVI.MixedADLogDensityProblem,
+    SupportedArray{<:IEEEFloat,1},
 }
 
 end
