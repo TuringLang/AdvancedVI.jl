f81dd9e49bfa8f66e71cb6bcaf446aab434eb516
Kyurae Kim <kyrkim@seas.upenn.edu>
2025-07-30 18:57:23 -0400

    fix add missing extensions

---
+++ ext/AdvancedVIMooncakeExt.jl
@@ -0,0 +1,14 @@
+module AdvancedVIMooncakeExt
+
+using AdvancedVI
+using Base: IEEEFloat
+using LogDensityProblems
+using Mooncake
+
+Mooncake.@from_rrule Mooncake.DefaultCtx Tuple{
+    typeof(LogDensityProblems.logdensity),
+    AdvancedVI.MixedADLogDensityProblem,
+    Array{<:IEEEFloat,1},
+}
+
+end
