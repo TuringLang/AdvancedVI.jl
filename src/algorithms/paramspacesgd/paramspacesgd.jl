e332d8c87348b7d39529508edc96b7fbda9e72aa
Kyurae Kim <kyrkim@seas.upenn.edu>
2025-09-15 12:04:53 -0400

    remove the type `ParamSpaceSGD`

--- src/algorithms/paramspacesgd/paramspacesgd.jl
+++ src/algorithms/paramspacesgd/paramspacesgd.jl
@@ -1,68 +1,9 @@
 
-"""
-    ParamSpaceSGD(
-        objective::AbstractVariationalObjective,
-        adtype::ADTypes.AbstractADType,
-        optimizer::Optimisers.AbstractRule,
-        averager::AbstractAverager,
-        operator::AbstractOperator,
-    )
-
-This algorithm applies stochastic gradient descent (SGD) to the variational `objective` over the (Euclidean) space of variational parameters.
-
-The trainable parameters in the variational approximation are expected to be extractable through `Optimisers.destructure`.
-This requires the variational approximation to be marked as a functor through `Functors.@functor`.
-
-!!! note
-    Different objective may impose different requirements on `adtype`, variational family, `optimizer`, and `operator`. It is therefore important to check the documentation corresponding to each specific objective. Essentially, each objective should be thought as forming its own unique algorithm.
-
-# Arguments
-- `objective`: Variational Objective.
-- `adtype`: Automatic differentiation backend. 
-- `optimizer`: Optimizer used for inference.
-- `averager` : Parameter averaging strategy.
-- `operator` : Operator applied to the parameters after each optimization step.
-
-# Output
-- `q_averaged`: The variational approximation formed from the averaged SGD iterates.
-
-# Callback
-The callback function `callback` has a signature of
-
-    callback(; rng, iteration, restructure, params, averaged_params, restructure, gradient)
-
-The arguments are as follows:
-- `rng`: Random number generator internally used by the algorithm.
-- `iteration`: The index of the current iteration.
-- `restructure`: Function that restructures the variational approximation from the variational parameters. Calling `restructure(params)` reconstructs the current variational approximation. 
-- `params`: Current variational parameters.
-- `averaged_params`: Variational parameters averaged according to the averaging strategy.
-- `gradient`: The estimated (possibly stochastic) gradient.
-
-"""
-struct ParamSpaceSGD{
-    Obj<:AbstractVariationalObjective,
-    AD<:ADTypes.AbstractADType,
-    Opt<:Optimisers.AbstractRule,
-    Avg<:AbstractAverager,
-    Op<:AbstractOperator,
-} <: AbstractVariationalAlgorithm
-    objective::Obj
-    adtype::AD
-    optimizer::Opt
-    averager::Avg
-    operator::Op
-end
-
-struct ParamSpaceSGDState{P,Q,GradBuf,OptSt,ObjSt,AvgSt}
-    prob::P
-    q::Q
-    iteration::Int
-    grad_buf::GradBuf
-    opt_st::OptSt
-    obj_st::ObjSt
-    avg_st::AvgSt
-end
+const ParamSpaceSGD = Union{
+    <:KLMinRepGradDescent,
+    <:KLMinRepGradProxDescent,
+    <:KLMinScoreGradDescent,
+}
 
 function init(rng::Random.AbstractRNG, alg::ParamSpaceSGD, q_init, prob)
     (; adtype, optimizer, averager, objective, operator) = alg
@@ -76,7 +17,15 @@ function init(rng::Random.AbstractRNG, alg::ParamSpaceSGD, q_init, prob)
     obj_st = init(rng, objective, adtype, q_init, prob, params, re)
     avg_st = init(averager, params)
     grad_buf = DiffResults.DiffResult(zero(eltype(params)), similar(params))
-    return ParamSpaceSGDState(prob, q_init, 0, grad_buf, opt_st, obj_st, avg_st)
+    if alg isa KLMinRepGradDescent
+        return KLMinRepGradDescentState(prob, q_init, 0, grad_buf, opt_st, obj_st, avg_st)
+    elseif alg isa KLMinRepGradProxDescent
+        return KLMinRepGradProxDescentState(prob, q_init, 0, grad_buf, opt_st, obj_st, avg_st)
+    elseif alg isa KLMinScoreGradDescent
+        return KLMinScoreGradDescentState(prob, q_init, 0, grad_buf, opt_st, obj_st, avg_st)
+    else
+        nothing
+    end
 end
 
 function output(alg::ParamSpaceSGD, state)
@@ -104,9 +53,21 @@ function step(
     params = apply(operator, typeof(q), opt_st, params, re)
     avg_st = apply(averager, avg_st, params)
 
-    state = ParamSpaceSGDState(
-        prob, re(params), iteration, grad_buf, opt_st, obj_st, avg_st
-    )
+    state = if alg isa KLMinRepGradDescent
+        KLMinRepGradDescentState(
+            prob, re(params), iteration, grad_buf, opt_st, obj_st, avg_st
+        )
+    elseif alg isa KLMinRepGradProxDescent
+        KLMinRepGradProxDescentState(
+            prob, re(params), iteration, grad_buf, opt_st, obj_st, avg_st
+        )
+    elseif alg isa KLMinScoreGradDescent
+        KLMinScoreGradDescentState(
+            prob, re(params), iteration, grad_buf, opt_st, obj_st, avg_st
+        )
+    else
+        nothing
+    end
 
     if !isnothing(callback)
         averaged_params = value(averager, avg_st)
